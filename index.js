8f9b1a570889093fdf8266254d92fc4d7332e617
aneeshseth <aneeshseth2018@gmail.com>
2023-10-31 21:37:45 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3009
+
+app.get("/", (req,res) => {
+    res.send("hey aneessh!!!")
+})
+
+app.listen(port, () => {
+    console.log(`app runniung on port ${port}`)
+})
